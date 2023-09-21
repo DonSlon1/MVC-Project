9b826bca48d38d776951c2e1d3f5f5feba264d59
Lukáš Dihel <lukindihel@gmail.com>
2023-09-21 18:43:37 +0200

    new functions for config manager

--- Core/Utils/Config/Manager.php
+++ Core/Utils/Config/Manager.php
@@ -2,6 +2,7 @@
 
 namespace Core\Utils\Config;
 
+use RuntimeException;
 use Core\Utils\{
     File\Manager as FileManager,
 };
@@ -11,12 +12,27 @@ class Manager
     private ?array $data = null;
     private string $configPath = '../App/Config/config.php';
     private readonly FileManager $fileManager;
+    private array $changedData = [];
+    protected array $associativeArrayAttributeList = [
+        'currencyRates',
+        'database',
+        'logger',
+        'defaultPermissions',
+    ];
     public function __construct()
     {
         $this->fileManager = new FileManager($this);
     }
 
-    public function get(string $name, $default = null): mixed
+    /**
+     * Get a config value.
+     *
+     * @param string $name Name of the config value.
+     * @param mixed|null $default Default value if some name dont exist.
+     *
+     * @return mixed
+     */
+    public function get(string $name, mixed $default = null): mixed
     {
         $keys = explode('.', $name);
 
@@ -68,5 +84,56 @@ class Manager
         $this->data = $this->fileManager->getPhpContents($this->configPath);
     }
 
+    /**
+     * Update the config data.
+     */
+    public function update():void
+    {
+        $this->data = $this->fileManager->getPhpContents($this->configPath);
+        $this->changedData = [];
+    }
+
+    /**
+     * Set a config value.
+     *
+     * @param string $name Name of the config value.
+     * @param mixed $value Value of the config parameter.
+     */
+    public function set(string $name, mixed $value): void
+    {
+        if (in_array($name, $this->associativeArrayAttributeList)) {
+            $value = (array) $value;
+        }
+        $this->changedData[$name] = $value;
+    }
+
+    /**
+     * Set multiple config values.
+     *
+     * @param array $data
+     */
+    public function setMultiple(array $data): void
+    {
+        foreach ($data as $name => $value) {
+            $this->set($name, $value);
+        }
+    }
+
+    /**
+     * Save the config  to config file.
+     *
+     * @throws RuntimeException
+     */
+    public function save(): void
+    {
+        $data = $this->fileManager->getPhpContents($this->configPath);
+        if (!is_array($data)) {
+            throw new RuntimeException("Could not read config.");
+        }
+        $data = array_merge($data, $this->changedData);
+        $this->fileManager->putPhpContents($this->configPath, $data);
+
+        $this->update();
+    }
 
 }

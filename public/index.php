ae7906368112e31c78f88b6455d84d2dbd710f82
Lukáš Dihel <lukindihel@gmail.com>
2023-09-21 22:55:04 +0200

    creating routs from json file

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php
 require '../vendor/autoload.php';
 
+use Core\Enums\HttpMethods;
+$fileManager = new Core\Utils\File\Manager(new Core\Utils\Config\Manager());
 
+$jsonRoute = $fileManager->getJsonContents('../App/Resources/routes.json');
 $router = new Core\Router();
-$router->add( '{controller}');
-$router->add( 'api/{controller}',['action'=>'send']);
-$router->add( '{controller}/{action}');
-$router->add( '{controller}/{action}/{year}/{month}');
+foreach ($jsonRoute as $route) {
+
+    if (HttpMethods::tryFrom($route['method']) !== null)
+        $method = HttpMethods::from($route['method']);
+    else
+        $method = HttpMethods::GET;
+    $router->add($route['route'], $route['params'], $method);
+}
 
 $router->dispatch($_SERVER['QUERY_STRING'], Core\Enums\HttpMethods::from($_SERVER['REQUEST_METHOD']));

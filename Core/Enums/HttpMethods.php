bafebe5aa6de409c1fc5d29670b6cb94163b3ef4
Lukáš Dihel <lukindihel@gmail.com>
2023-09-21 22:54:44 +0200

    Enable creating Enums with lower case

--- Core/Enums/HttpMethods.php
+++ Core/Enums/HttpMethods.php
@@ -4,9 +4,12 @@ namespace Core\Enums;
 
 enum HttpMethods: string
 {
-    case GET = 'GET';
-    case POST = 'POST';
-    case PUT = 'PUT';
-    case  DELETE = 'DELETE';
+    case GET = 'GET'|'get';
+    case POST = 'POST'|'post';
+    case PUT = 'PUT'|'put';
+    case  DELETE = 'DELETE'|'delete';
+    case PATCH = 'PATCH'|'patch';
+    case OPTIONS = 'OPTIONS'|'options';
+
 
 }
